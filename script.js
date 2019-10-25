d060450aab93fc4482304078fddeccfdde69e95f
Kam Kaiserman <43228122+kamkaiserman@users.noreply.github.com>
2019-10-24 20:51:45 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-import { films } from 'films.js'
-import { people } from 'people.js'
+import { films } from './films.js'
+import { people } from './people.js'
 
-let mainArea = document.queerySelector('main')
+let mainArea = document.querySelector('main')
 
-films.forEach(function(film) {
+/* films.forEach(function(film) {
     let filmDiv = document.createElement('div')
     let filmTitle = document.createElement('h1')
     let filmCrawl = document.createElement('p')
 
     filmTitle.textContent = film.title
-    filmCrawl.textContent = film.opeing_crawl
+    filmCrawl.textContent = film.opening_crawl
 
     filmDiv.appendChild(filmTitle)
     filmDiv.appendChild(filmCrawl)
 
     mainArea.appendChild(filmDiv)
-});
+}); */
 
-people.forEach(function(person) {
+people.forEach((person) => {
     let personDiv = document.createElement('div')
-    let name = document.createElement('h1')
+    let name = document.createElement('h3')
     let gender = document.createElement('p')
     let pic = document.createElement('img')
 
-    getCharNumber(person.url)
+    personDiv.setAttribute('class', 'charDivs')
+    pic.setAttribute('class', 'picDivs')
 
-    personTitle.textContent = person.name
-    personCrawl.textContent = person.gender
-    pic.src = `https://starwars-visualguide.com/assets/img/charaters/${charNum}`
+    let charNum = getCharNumber(person.url)
+
+    name.textContent = person.name
+    gender.textContent = `Gender: ${person.gender}`
+    pic.textContent = person.pic
+    pic.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
 
     personDiv.appendChild(name)
     personDiv.appendChild(gender)
@@ -40,6 +44,50 @@ function getCharNumber(charURL) {
     let end = charURL.lastIndexOf('/')
     let charID = charURL.substring(end - 2, end)
     if (charID.indexOf('/') !== -1) {
-        console.log(charID.slice0, 1)
+        return charID.slice(1, 2)
+    } else {
+        return charID
     }
 }
+
+const allDivs = Array.from(document.querySelectorAll('div'))
+console.log(allDivs)
+const mainHeader = document.querySelector('header')
+let maleButton = document.createElement('button')
+maleButton.textContent = 'Misogyny HAS WON'
+
+maleButton.addEventListener('click', () => {
+    femaleCharacters.forEach(character => {
+        let matchedDiv = allDivs.find(oneDiv => {
+            return oneDiv.firstChild.textContent === character.name
+        })
+        matchedDiv.setAttribute("style", "display: none;")
+    })
+    maleCharacters.forEach((item) => {
+        console.log(item)
+    })
+})
+
+let femaleButton = document.createElement('button')
+femaleButton.textContent = 'LET THE WOMEN VOTE'
+
+femaleButton.addEventListener('click', () => {
+    femaleCharacters.forEach(character => {
+        let matchedDiv = allDivs.find(oneDiv => {
+            return oneDiv.firstChild.textContent === character.name
+        })
+        matchedDiv.setAttribute("style", "display: revert;")
+    })
+    maleCharacters.forEach((item) => {
+        console.log(item)
+    })
+})
+
+mainHeader.appendChild(maleButton)
+mainHeader.appendChild(femaleButton)
+
+const maleCharacters = people.filter(person => person.gender === 'male')
+const femaleCharacters = people.filter(person => person.gender === 'female')
+
+console.log(maleCharacters)
+console.log(femaleCharacters)
