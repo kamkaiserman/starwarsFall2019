b4b99d7f58e09b35f7d4b383b4b9687294cb8a71
Kam Kaiserman <43228122+kamkaiserman@users.noreply.github.com>
2019-12-11 18:25:42 -0700

    Updated
    Buttons now only display one category and colors have changed.

--- script.js
+++ script.js
@@ -22,6 +22,10 @@ people.forEach((person) => {
     let name = document.createElement('h3')
     let gender = document.createElement('p')
     let pic = document.createElement('img')
+    let birthday = document.createElement('p')
+    let mass = document.createElement('p')
+    let hair = document.createElement('p')
+
 
     personDiv.setAttribute('class', 'charDivs')
     pic.setAttribute('class', 'picDivs')
@@ -29,13 +33,21 @@ people.forEach((person) => {
     let charNum = getCharNumber(person.url)
 
     name.textContent = person.name
-    gender.textContent = `Gender: ${person.gender}`
     pic.textContent = person.pic
     pic.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+    gender.textContent = `Gender: ${person.gender}`
+    hair.textContent = `Hair Color: ${person.hair_color}`
+    mass.textContent = `Weight: ${person.mass}`
+    birthday.textContent = `Birth Year: ${person.birth_year}`
+
 
     personDiv.appendChild(name)
     personDiv.appendChild(gender)
     personDiv.appendChild(pic)
+    personDiv.appendChild(hair)
+    personDiv.appendChild(mass)
+    personDiv.appendChild(birthday)
+
 
     mainArea.appendChild(personDiv)
 });
@@ -63,29 +75,79 @@ maleButton.addEventListener('click', () => {
         })
         matchedDiv.setAttribute("style", "display: none;")
     })
-    maleCharacters.forEach((item) => {
-        console.log(item)
+    otherCharacters.forEach(character => {
+        let matchedDiv = allDivs.find(oneDiv => {
+            return oneDiv.firstChild.textContent === character.name
+        })
+        matchedDiv.setAttribute("style", "display: none;")
     })
+    maleCharacters.forEach(character => {
+        let matchedDiv = allDivs.find(oneDiv => {
+            return oneDiv.firstChild.textContent === character.name
+        })
+        matchedDiv.setAttribute("style", "display: grid;")
+    })
+    headline.setAttribute("style", "color: rgba(255, 255, 255, 0.1)")
+    window.scrollTo(0, 435);
 })
 
 let femaleButton = document.createElement('button')
 femaleButton.textContent = 'LET THE WOMEN VOTE'
 
 femaleButton.addEventListener('click', () => {
+    otherCharacters.forEach(character => {
+        let matchedDiv = allDivs.find(oneDiv => {
+            return oneDiv.firstChild.textContent === character.name
+        })
+        matchedDiv.setAttribute("style", "display: none;")
+    })
+    maleCharacters.forEach(character => {
+        let matchedDiv = allDivs.find(oneDiv => {
+            return oneDiv.firstChild.textContent === character.name
+        })
+        matchedDiv.setAttribute("style", "display: none;")
+    })
     femaleCharacters.forEach(character => {
         let matchedDiv = allDivs.find(oneDiv => {
             return oneDiv.firstChild.textContent === character.name
         })
-        matchedDiv.setAttribute("style", "display: revert;")
+        matchedDiv.setAttribute("style", "display: grid;")
     })
-    maleCharacters.forEach((item) => {
-        console.log(item)
+    headline.setAttribute("style", "color: rgba(255, 255, 255, 0.1)")
+    window.scrollTo(0, 435);
+})
+
+let otherButton = document.createElement('button')
+otherButton.textContent = 'THE A.I. IS EVOLVING'
+
+otherButton.addEventListener('click', () => {
+    femaleCharacters.forEach(character => {
+        let matchedDiv = allDivs.find(oneDiv => {
+            return oneDiv.firstChild.textContent === character.name
+        })
+        matchedDiv.setAttribute("style", "display: none;")
     })
+    maleCharacters.forEach(character => {
+        let matchedDiv = allDivs.find(oneDiv => {
+            return oneDiv.firstChild.textContent === character.name
+        })
+        matchedDiv.setAttribute("style", "display: none;")
+    })
+    otherCharacters.forEach(character => {
+        let matchedDiv = allDivs.find(oneDiv => {
+            return oneDiv.firstChild.textContent === character.name
+        })
+        matchedDiv.setAttribute("style", "display: grid;")
+    })
+    headline.setAttribute("style", "color: rgba(255, 255, 255, 0.1)")
+    window.scrollTo(0, 435);
 })
 
 mainHeader.appendChild(maleButton)
 mainHeader.appendChild(femaleButton)
+mainHeader.appendChild(otherButton)
 
+const otherCharacters = people.filter(person => person.gender != 'male' && person.gender != 'female')
 const maleCharacters = people.filter(person => person.gender === 'male')
 const femaleCharacters = people.filter(person => person.gender === 'female')
 
